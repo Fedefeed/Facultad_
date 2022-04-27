b1910425e6de90d4276824b38c975b9862d0a6c6
Fedefeed <54046912+Fedefeed@users.noreply.github.com>
2022-04-27 09:42:09 -0300

    Add files via upload

---
+++ Carpeta_Modulos/Llamada_al_Modulo.py
@@ -0,0 +1,11 @@
+#Primera referencia
+#Para resumir la llamada
+import Modulos as M
+#import Modulos
+M.sumar(2,2)
+M.potencia3(2)
+#Segunda referencia
+#Para llamar a todas las funciones y no una en particular
+#from Modulos import*
+from Modulos import restar
+restar(2,2)
---
+++ Carpeta_Modulos/Modulos.py
@@ -0,0 +1,17 @@
+def sumar(a,b):
+    return print("Suma: ", a+b)
+def restar(a,b):
+    return print("Resta: ", a-b)
+def multiplicar(a,b):
+    return print("Multiplicacion: ", a*b)
+def dividir(a,b):
+    if b==0:
+        return print("No se puede dividir por 0")
+    else:
+        return a/b
+def potencia2(a):
+    return print("Potencia al cuadrado: ", a**2)
+def potencia3(a):
+    return print("Potencia al cubo: ", a**3)
+def potencia4(a):
+    return print("Potencia a la cuarta: ", a**4)
