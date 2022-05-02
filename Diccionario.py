9b33e869af7b45ee2e5e09ec59ef04523a1d95e4
Fedefeed <54046912+Fedefeed@users.noreply.github.com>
2022-05-02 17:42:13 -0300

    Add files via upload

---
+++ Diccionario.py
@@ -0,0 +1,7 @@
+#(){}[]
+propiedades={1:"Nombre", 2:"Apellido"}
+propiedades[3]="Edad"
+propiedades[3]="DNI"
+print(propiedades)
+actores={"Nombre":"Luis","Obra":"San Diego","edad":20}
+print(actores)
